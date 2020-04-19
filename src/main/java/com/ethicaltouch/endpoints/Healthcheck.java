203e36d08e95ba6741f241b336d33779dd33aad2
valentin99dumitrache@gmail.com <valentindumitrache@MacBook-Pro-Valentin.local>
2020-04-19 03:08:12 +0300

    implement database test part 3

--- src/main/java/com/ethicaltouch/endpoints/Healthcheck.java
+++ src/main/java/com/ethicaltouch/endpoints/Healthcheck.java
@@ -2,7 +2,7 @@ package com.ethicaltouch.endpoints;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-@Path("healthcheck")
+@Path("")
 public class Healthcheck {
     @GET
     public String getIt() {
--- src/main/java/com/ethicaltouch/postgresql/Main.java
+++ src/main/java/com/ethicaltouch/postgresql/Main.java
@@ -14,23 +14,22 @@ import java.sql.Statement;
 public class Main {
 
     @Autowired
-    private static DataSource dataSource;
+    private DataSource dataSource;
 
     public static void init() {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Main.class.getPackage().getName());
     }
 
     @PostConstruct
-    public static String myRealMainMethod() throws SQLException {
+    public void myRealMainMethod() throws SQLException {
         Statement stmt = dataSource.getConnection().createStatement();
         stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
         stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
         stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
         ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
         while (rs.next()) {
-            return "Read from DB: " + rs.getTimestamp("tick");
+            System.out.println("Read from DB: " + rs.getTimestamp("tick"));
         }
-        return "Didn't work";
     }
 
 }
