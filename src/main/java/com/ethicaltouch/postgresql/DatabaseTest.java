dd5f42aeec3896f3725bf319225f5c230ec7d194
valentin99dumitrache@gmail.com <valentindumitrache@MacBook-Pro-Valentin.local>
2020-04-19 03:51:12 +0300

    implement database test part 4

--- src/main/java/com/ethicaltouch/postgresql/DatabaseTest.java
+++ src/main/java/com/ethicaltouch/postgresql/DatabaseTest.java
@@ -5,6 +5,9 @@ import java.sql.SQLException;
 import static com.ethicaltouch.postgresql.PostgresqlConnection.getConnection;
 
 public class DatabaseTest {
+    public static void main(String[] args) {
+        init();
+    }
     public static void init() {
         try {
             getConnection().createStatement().executeQuery("CREATE TABLE TEST (test varchar)");
