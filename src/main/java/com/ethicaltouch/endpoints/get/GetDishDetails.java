ef07784b715539649dfb3ff58941e016eafbb41d
valentin99dumitrache@gmail.com <valentindumitrache@MacBook-Pro-Valentin.local>
2020-04-22 02:35:10 +0300

    refactor endpoints part 5

--- src/main/java/com/ethicaltouch/endpoints/get/GetDishDetails.java
+++ src/main/java/com/ethicaltouch/endpoints/get/GetDishDetails.java
@@ -3,6 +3,7 @@ package com.ethicaltouch.endpoints.get;
 import com.ethicaltouch.QueryExecutor;
 import com.ethicaltouch.resources.Cook;
 import com.ethicaltouch.resources.Dish;
+import com.google.gson.Gson;
 
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.GET;
@@ -19,6 +20,9 @@ import static com.ethicaltouch.QueryExecutor.closeConnection;
 
 @Path("getdishdetails")
 public class GetDishDetails {
+    public static void main(String[] args) {
+        System.out.println(getDishDetails("1a", "1a"));
+    }
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public static Response getDishDetails(@NotNull @QueryParam("dishid") String dishId, @NotNull @QueryParam("cookid") String cookId) {
@@ -26,18 +30,20 @@ public class GetDishDetails {
         try {
             List<String> ingredients = new ArrayList<>();
             List<String> pictures = new ArrayList<>();
-
-            ResultSet resultSet = QueryExecutor.init("SELECT * FROM dish WHERE dish_id='" + dishId + "'");
-            Dish dishWithoutDetails = new Dish(
-                    resultSet.getString("id"),
-                    resultSet.getString("name"),
-                    resultSet.getInt("price"),
-                    resultSet.getString("main_picture"),
-                    resultSet.getString("description"),
-                    resultSet.getString("cook_id")
-            );
+            Dish dishWithoutDetails = null;
+            Cook cook = null;
+            ResultSet resultSet = QueryExecutor.init("SELECT * FROM dish WHERE id='" + dishId + "'");
+            while (resultSet.next()) {
+                dishWithoutDetails = new Dish(
+                        resultSet.getString("id"),
+                        resultSet.getString("name"),
+                        resultSet.getInt("price"),
+                        resultSet.getString("main_picture"),
+                        resultSet.getString("description"),
+                        resultSet.getString("cook_id")
+                );
+            }
             closeConnection();
-
             resultSet = QueryExecutor.init("SELECT picture_url FROM dish_picture WHERE dish_id='" + dishId + "'");
             while (resultSet.next()) {
                 pictures.add(resultSet.getString("picture_url"));
@@ -50,22 +56,24 @@ public class GetDishDetails {
             }
             closeConnection();
 
-            resultSet = QueryExecutor.init("SELECT * FROM cook WHERE cook_id='" + cookId + "'");
-            Cook cook = new Cook(
-                    cookId,
-                    resultSet.getString("first_name"),
-                    resultSet.getString("last_name"),
-                    resultSet.getString("city"),
-                    resultSet.getString("county"),
-                    resultSet.getString("description"),
-                    resultSet.getString("cover_picture"),
-                    resultSet.getString("profile_picture"),
-                    resultSet.getString("phone_number"),
-                    null
-            );
+            resultSet = QueryExecutor.init("SELECT * FROM cook WHERE id='" + cookId + "'");
+            while(resultSet.next()) {
+                cook = new Cook(
+                        cookId,
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getString("city"),
+                        resultSet.getString("county"),
+                        resultSet.getString("description"),
+                        resultSet.getString("cover_picture"),
+                        resultSet.getString("profile_picture"),
+                        resultSet.getString("phone_number"),
+                        null
+                );
+            }
             Dish dish = new Dish(dishWithoutDetails, pictures, ingredients, cook);
             response = Response.status(Response.Status.OK)
-                    .entity(dish)
+                    .entity(new Gson().toJson(dish))
                     .header("Access-Control-Allow-Origin", "*")
                     .build();
             closeConnection();
